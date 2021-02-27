b089a351f035cb8343751ac23329bf15a2646415
a4anthony <55565463+a4anthony@users.noreply.github.com>
2021-02-27 20:58:45 +0100

    project setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/tailwind.css";
+import Amplify from "aws-amplify";
+import "@aws-amplify/ui-components";
+import aws_exports from "./aws-exports";
 
-createApp(App).use(store).use(router).mount('#app')
+Amplify.configure(aws_exports);
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
